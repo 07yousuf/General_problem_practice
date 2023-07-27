465d3b3a6ce5c4860c8c8955330327257896feb9
yousuf <ysyousuf500@gmail.com>
2023-07-27 19:17:44 +0600

    initial

---
+++ SubtractionMatrix7.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,column,i,j;
+    cin>>row>>column;
+    int numbers[row][column],numbers2[row][column],add[column][column];
+    for(i=0; i<row; i++){
+        for(j=0; j<column; j++){
+            cin>>numbers[i][j];
+        }
+    }
+    cout<<"Second matrix: "<<endl;
+    for(i=0; i<row; i++){
+        for(j=0; j<column; j++){
+            cin>>numbers2[i][j];
+        }
+    }
+    for(i=0; i<row; i++){
+        for(j=0; j<column; j++){
+           add[i][j]=numbers[i][j] - numbers2[i][j];
+        }
+    }
+    cout<<"Subtraction of two matrix:"<<endl;
+    for(i=0; i<row; i++){
+        for(j=0; j<column; j++){
+           cout<<add[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
